77a922f959537e6c75f73de60d159d52dd2b7446
渡辺雄貴 <watanabeyuuki@watanabeyuuki-no-macbook-air.local>
2016-06-13 17:58:50 +0900

    Initial Commit

---
+++ 検定アプリ/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  検定アプリ
+//
+//  Created by 渡辺雄貴 on 2016/06/13.
+//  Copyright © 2016年 渡辺雄貴. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
